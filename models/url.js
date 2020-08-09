4a07865c853c373bbfee0cf1b66a9fa7d9aabf42
chidibede <chidibede@gmail.com>
2020-08-09 12:47:37 +0100

    Initial deployment

---
+++ models/url.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+    original_url: {type: String, required: true},
+    short_url: {type: String, required: true, unique: true}
+})
+
+const Url = mongoose.model('Url', urlSchema)
+
+module.exports = Url
--- server.js
+++ server.js
@@ -1,37 +1,91 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require("express");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const Url = require("./models/url");
+const cors = require("cors");
+const dns = require("dns");
+const shortid = require("shortid");
+const { error } = require("console");
 
-var cors = require('cors');
+const app = express();
 
-var app = express();
+// Basic Configuration
+const port = process.env.PORT || 3002;
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
+/** this project needs a db !! **/
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(
+  process.env.DB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  (error) => {
+    if (error) {
+      console.log("Error connecting to mongo atlas", error);
+    } else {
+      console.log("Connected to mongo atlas successfully...");
+    }
+  }
+);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.post("/api/shorturl/new", async function (req, res, next) {
+  dns.lookup(req.body.url, async (error, address, family) => {
+    if (error) {
+      res.json({ error: "invalid url" });
+    } else {
+      await Url.create(
+        { original_url: req.body.url, short_url: shortid.generate() },
+        (error, data) => {
+          if (error) {
+            console.log("Error creating document", error);
+          } else {
+            const url_shortened = {
+              original_url: data.original_url,
+              short_url: data.short_url,
+            };
+            res.json(url_shortened);
+            next();
+          }
+        }
+      );
+    }
+  });
 });
 
+app.get("/api/shorturl/new/:shortUrl", async function (req, res) {
+  var shortUrlCode = req.params.shortUrl;
+  await Url.findOne({ short_url: shortUrlCode }, (error, data) => {
+    if (error) {
+      console.log("Error finding url");
+    }else{
+      if (!data) {
+        return res
+        .status(400)
+        .json("The short url doesn't exists in our system.");
+        
+    } else {
+      return res.redirect("https://" + data.original_url)
+    }
+    }
+  })
+});
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+app.listen(port, () => {
+  console.log(`Server running on localhost:${port}`);
 });
